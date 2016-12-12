25717c3af7a34eac17fe76fcd01c809f55c5788d
sushiljainam <sushiljain1993@gmail.com>
2016-12-12 14:28:05 +0530

    added request handling php file - to save incoming JSON data from tt-edit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set("allow_url_fopen", true);
+$postdata = json_decode(file_get_contents("php://input"));
+
+
+$json = array();
+    $bus = array(
+        'success' => false,
+        'rTL' => false,
+        'rcvd52' => $postdata,
+    );
+    array_push($json, $bus);
+
+$jsonstring = json_encode($bus);
+echo $jsonstring;
+
+?>
