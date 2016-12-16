62b23c7939efcd54f647e3334d16737893df4145
sushiljainam <sushiljain1993@gmail.com>
2016-12-16 14:38:51 +0530

    new file for ..query4

--- dbQuery.php
+++ dbQuery.php
@@ -18,7 +18,7 @@ getTeachers(*)
 getTeachers(*deptt)
 getSubjects(course,branch,sem)
 getRows(*)
-getRows(*config) config: (userType,teacher | userType,room | userType,course,branch,sem) userType:[t|r|s]
+getRows(*config) config: (userType,teacher.key | userType,room.key | userType,course.key,branch.key,sem) userType:[t|r|s]
 ...
 set..All
 add..All
@@ -26,5 +26,16 @@ update..All
 ...
 try to write all and generalized queries at single place
 and a single fxn to execute any query.
+
+
+default values: consider if not/wrong given
+course: bTech
+branch: CS
+userType: s
+
+for future: can ignore coding about these
+roomGroup,deptt(teacher)
+
+:all keys are case sensitive
 */
 ?>
