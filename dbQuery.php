656257095e890f3623a3c26a60718d6e8793d057
sushiljainam <sushiljain1993@gmail.com>
2016-12-16 11:20:08 +0530

    new file for ..query3

--- dbQuery.php
+++ dbQuery.php
@@ -20,5 +20,11 @@ getSubjects(course,branch,sem)
 getRows(*)
 getRows(*config) config: (userType,teacher | userType,room | userType,course,branch,sem) userType:[t|r|s]
 ...
+set..All
+add..All
+update..All
+...
+try to write all and generalized queries at single place
+and a single fxn to execute any query.
 */
 ?>
