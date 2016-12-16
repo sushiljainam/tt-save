325821d3a415fb1f1bffac14f2d3828eddf6ea28
sushiljainam <sushiljain1993@gmail.com>
2016-12-16 11:13:50 +0530

    new file for ..query2

--- dbQuery.php
+++ dbQuery.php
@@ -12,9 +12,13 @@ getBranches(course)
 getYearCount(course)
 getSemCount(course)
 getBatchCount(course,branch)
-getRooms()
-getTeachers()
+getRooms(*)
+getRooms(*roomGroup)
+getTeachers(*)
+getTeachers(*deptt)
 getSubjects(course,branch,sem)
+getRows(*)
+getRows(*config) config: (userType,teacher | userType,room | userType,course,branch,sem) userType:[t|r|s]
 ...
 */
 ?>
