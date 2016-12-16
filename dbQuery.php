5f1d55b12f8ee1f9e5200e19b0f7a7de9b882dfd
sushiljainam <sushiljain1993@gmail.com>
2016-12-16 11:01:47 +0530

    new file for ..query

---
+++ dbQuery.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * All sqlite dbQueries here in this file
+ * SELECT | INSERT | DELETE | UPDATE (curd)
+ *
+ */
+
+//list
+/*
+getCourses()
+getBranches(course)
+getYearCount(course)
+getSemCount(course)
+getBatchCount(course,branch)
+getRooms()
+getTeachers()
+getSubjects(course,branch,sem)
+...
+*/
+?>
